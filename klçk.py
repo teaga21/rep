6879059d6af1604f8f008f3cf615b716f42bdee1
teaga21 <th4445350@gmail.com>
2026-02-09 22:31:33 -0300

    Add files via upload

---
+++ klçk.py
@@ -0,0 +1,11 @@
+import os
+os.system('cls')
+
+idade=int(input("Qual a sua idade?:"))
+nome=input("Qual o seu nome?:")
+
+estado_civil=input("Qual o seu estado civil?")
+
+print(idade)
+print(nome)
+print(estado_civil)
